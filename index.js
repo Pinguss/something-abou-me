b09976481b2db256d784a47dd51d1b1f6fc5d3a5
Pinguss <47196337+Pinguss@users.noreply.github.com>
2019-03-02 07:29:15 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+
+  $(".btn").click( function(){
+
+    var number = Math.floor(Math.random() * 101);
+    $("h1").text(number + "% láska!");
+
+    if (number < 30){
+      var vysledek = "Tvá láska k feldě míří k bodu mrazu... 😢";
+    }
+
+    else if (number >= 30 && number < 50){
+      vysledek = "Pozor! Tvá láska k feldě nejspíš prochází krizí! Je třeba jí posílit! 😉";
+    }
+
+    else if (number >= 50 && number < 80){
+      vysledek = "Máš feldu moc rád/a! Jen tak dál! 😊";
+      }
+
+    else{
+      vysledek = "Úžasný! Ty feldu skutečně miluješ! Jsi nefalšovaný felďácký milovník! 😍";
+      const playSound = new Audio("audio/felicie.mp3").play();
+      $(".formular").remove();
+      $(".feldaDrift img").attr("src","http://zonderpump.com/images/skoda-felicia.jpg");
+      }
+
+  $("p").text(vysledek);
+  });
